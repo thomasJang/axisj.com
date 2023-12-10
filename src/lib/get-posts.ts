c40e0d88175efbb83d83e71559eb2aef889efecc
Thomas <tom@axisj.com>
2023-12-10 20:29:46 +0900

    fix path resolve

--- src/lib/get-posts.ts
+++ src/lib/get-posts.ts
@@ -7,7 +7,8 @@ import { cache } from "react";
 const thirdPartyPosts: Post[] = [];
 
 export const getPosts = cache(async (includeThirdPartyPosts?: boolean) => {
-  const posts = await fs.readdir("./src/posts/");
+  const postsPath = path.resolve(process.cwd(), "src", "posts");
+  const posts = await fs.readdir(postsPath);
 
   const postsWithMetadata = await Promise.all(
     posts
@@ -15,7 +16,7 @@ export const getPosts = cache(async (includeThirdPartyPosts?: boolean) => {
         (file) => path.extname(file) === ".md" || path.extname(file) === ".mdx"
       )
       .map(async (file) => {
-        const filePath = `./src/posts/${file}`;
+        const filePath = path.resolve(process.cwd(), "src", "posts", file);
         const postContent = await fs.readFile(filePath, "utf8");
         const { data, content } = matter(postContent);
 
